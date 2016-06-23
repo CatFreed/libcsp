a81e22a865b755b6dbeed9e6909a8142bf71b913
Catherine Freed <Catherine.Freed@gmail.com>
2016-06-22 21:34:26 -0500

    Basic code done, pending good compile.

--- src/drivers/usart/usart_kubosHAL.c
+++ src/drivers/usart/usart_kubosHAL.c
@@ -1,17 +1,17 @@
-//Common includes
 #include <stdio.h>
 #include <csp/drivers/usart.h>
 #include <csp/arch/csp_thread.h>
 #include <csp/csp.h>
 #include <kubos-hal/uart.h>
 
-//HAL-specific includes
-
 //Function Initialization
 
 //Variables
+usart_callback_t usart_callback = NULL;
 
-//Actual code starts here
+//TODO: Move this to somewhere so that multiple UART ports can be open and communicating at the same time.
+// This is how the Windows and Linux drivers currently do it, though.
+KUARTNum uart_num;
 
 /*Common functions
  * putstr
@@ -20,7 +20,7 @@
  * init
  */
 void usart_putstr(char* buf, int len){
-	k_uart_write(uart_num, buf, len); //Replace with write or sendData function
+	k_uart_write(uart_num, buf, len);
 }
 
 void usart_putc(char c){
@@ -40,8 +40,27 @@ int usart_messages_waiting(int handle){
 	return k_uart_rx_queue_len((KUARTNUM) handle);
 }
 
+//TODO: If csp_thread_create gets converted to the direct FreeRTOS call, this definition will need to change
 CSP_DEFINE_TASK(uart_listen) {
-//TODO: this. While(1) read and stuff
+
+	int length;
+	char recvBuffer[300];
+
+	//Receive loop
+	//TODO: While(!exit) instead of while(1)?
+	while (1){
+
+		length = k_uart_read(uart_num, recvBuffer, 300);
+
+		if (length <=0){
+			log("Connection Closed");
+			exit(1);
+		}
+
+		if (usart_callback){
+			usart_callback(recvBuffer, length, NULL);
+		}
+	}
 }
 
 void usart_init(struct usart_conf * conf){
@@ -61,9 +80,10 @@ void usart_init(struct usart_conf * conf){
 	k_uart_init(uart_num, config);
 
 	//Start RX thread?  Do we have threads?
-	//NOTE: Assuming this is running on one of the libcsp architectures (probably FreeRTOS)
+	//NOTE: Assuming this is running on FreeRTOS
 	//      so csp_thread_create should allocate everything appropriately for me?
-	//      Seems like a fairly large assumption...
+	//      Seems like a fairly large assumption... The Windows and Linux drivers
+	//      both explicitly called their thread functions...
 	//      Copying parameters from the kiss.c example's SERVER/CLIENT threads
 	csp_thread_create(uart_listen, "uartRXThread", 1000, NULL, 0, &rxThread);
 }
